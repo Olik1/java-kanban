10d7ad1bd461c7a965928db7a0d9d30a9a87b3e3
Olik1 <Dorofeeva86@yandex.ru>
2022-12-27 20:30:12 +0300

    test: проверка истории просмотров

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,21 @@ public class Main {
         TaskManager taskManager = Managers.getDefault();
         Task task1 = new Task ("Задача №1", "Выполнить", Status.NEW);
         Task task2 = new Task("Задача №3", "Выполнить", Status.NEW);
+        taskManager.addNewTask(task1);
+        taskManager.addNewTask(task2);
+        taskManager.getTaskId(task1.getId());
+        taskManager.getTaskId(task1.getId());
         Epic epic1 = new Epic("Эпик №1", "Обязательно выполнить");
         Epic epic2 = new Epic("Эпик №2", "Обязательно выполнить");
         SubTask subTask1 = new SubTask("Подзадача №1",
-                "Выполнение не обязательно",Status.NEW, 1);
+                "Выполнение не обязательно",Status.NEW, epic1.getId());
         SubTask subTask2 = new SubTask("Подзадача №2",
-                "Выполнение не обязательно",Status.NEW, 1);
-        taskManager.addNewTask(task1);
-        taskManager.addNewTask(task2);
+                "Выполнение не обязательно",Status.NEW, epic2.getId());
         taskManager.addNewEpic(epic1);
         taskManager.addNewEpic(epic2);
         taskManager.addNewSubTask(subTask1);
         taskManager.addNewSubTask(subTask2);
+        taskManager.getEpicId(epic1.getId());
         System.out.println(taskManager.getHistory());
     }
 }
